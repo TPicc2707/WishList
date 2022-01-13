fd9ae3c2376724220b9f859fd75d6d77c44ea753
Tony Piccirilli <anpicc01@louisville.edu>
2022-01-13 15:51:33 -0500

    Seed Db on Startup and Register all services within application configure services

---
+++ src/Services/Person/Person.API/Extensions/HostExtensions.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Person.API.Extensions
+{
+    public static class HostExtensions
+    {
+        public static IHost MigrateDatabase<TContext>(this IHost host,
+                                              Action<TContext, IServiceProvider> seeder,
+                                              int? retry = 0) where TContext : DbContext
+        {
+            int retryForAvailability = retry.Value;
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+                var context = services.GetService<TContext>();
+
+                try
+                {
+                    logger.LogInformation("Migrating database with context {DbContextName}", typeof(DbContext).Name);
+
+                    InvokeSeeder(seeder, context, services);
+
+                    logger.LogInformation("Migrated database with context {DbContextName}", typeof(DbContext).Name);
+                }
+                catch (SqlException ex)
+                {
+                    logger.LogError(ex, "An error occurred while migrating the database using context {DbContextName}", typeof(DbContext).Name);
+
+                    if (retryForAvailability < 50)
+                    {
+                        retryForAvailability++;
+                        System.Threading.Thread.Sleep(2000);
+                        MigrateDatabase<TContext>(host, seeder, retryForAvailability);
+                    }
+
+                }
+            }
+
+            return host;
+        }
+
+        private static void InvokeSeeder<TContext>(Action<TContext, IServiceProvider> seeder, TContext context, IServiceProvider services) where TContext : DbContext
+        {
+            context.Database.Migrate();
+            seeder(context, services);
+        }
+
+    }
+}
---
+++ src/Services/Person/Person.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Person.Application.Contracts.Persistence;
+using Person.Infrastructure.Persistence;
+using Person.Infrastructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<PersonDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("PersonWishListConnectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<IPersonRepository, PersonRepository>();
+
+            return services;
+        }
+
+    }
+}
