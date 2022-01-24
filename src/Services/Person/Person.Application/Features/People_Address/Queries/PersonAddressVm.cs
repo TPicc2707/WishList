f735b66843c6111f33184ec16694ab83e6ddb6e9
Tony Piccirilli <anpicc01@louisville.edu>
2022-01-24 10:14:55 -0500

    PersonAddressVm for future reference

---
+++ src/Services/Person/Person.Application/Features/People_Address/Queries/PersonAddressVm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person.Application.Features.People_Address.Queries
+{
+    public class PersonAddressVm
+    {
+        public int Id { get; set; }
+        public int Person_Id { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+
+    }
+}
